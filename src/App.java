086e6983eb18fccfeb1d2412f9ece0631af0870c
PedroMozany <pedro.mozanny@hotmail.com>
2022-05-24 16:26:08 -0300

    Implementação da interação do jogo com usuario

--- src/App.java
+++ src/App.java
@@ -1,17 +1,28 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
-        Random random = new Random();
-        int number = random.nextInt(100);
+        Scanner sc = new Scanner(System.in);
+        int cont = 0, repeat = 5;
 
-        if (number % 2 == 0) {
-            System.out.println("Par");
-        }else{
-            System.out.println("Impar");
+        while (repeat != 0) {
+            System.out.println("type 0 for flip \n" + "type 1 for coin");
+            int resp = sc.nextInt();
+            if (resp == 0) {
+                System.out.println("Rigth!!");
+                cont++;
+                repeat--;
+            } else if (resp == 1) {
+                System.out.println("Rigth!!");
+                cont++;
+                repeat--;
+            }else{
+                System.out.println("Invalid option.");
+            }
         }
-
+        System.out.println("Pontuação:" + cont);
     }
 
 

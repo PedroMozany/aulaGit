097a22c8cb75a08a8b861ac3d35edbd17d52dc8a
PedroMozany <pedro.mozanny@hotmail.com>
2022-05-24 23:58:02 -0300

    Finalizado

--- src/App.java
+++ src/App.java
@@ -1,18 +1,40 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
-        Random random = new Random();
-        int number = random.nextInt(100);
+            Scanner sc = new Scanner(System.in);
+            int cont = 0, repeat = 5;
 
-        if (number % 2 == 0) {
-            System.out.println("Par");
-        }else{
-            System.out.println("Impar");
+            while (repeat != 0 ) {
+                System.out.println("type 0 for flip \ntype 1 for coin");
+                int resp = sc.nextInt();
+                int moeda = lançarMoeda();
+
+                if (resp == moeda) {
+                    System.out.println("Rigth!!\n");
+                    ++cont;
+                    --repeat;
+                } else {
+                    System.out.println("failed!!\n");
+                    --repeat;
+                }
+            }
+
+            System.out.println("Pontuação:" + cont);
         }
 
+        public static int lançarMoeda() {
+            Random random = new Random();
+            int number = random.nextInt(2);
+            if (number % 2 == 0) {
+                System.out.println("Result: flip");
+                return 0;
+            }else{
+                System.out.println("Result: coin");
+                return 1;
+            }
+
+        }
     }
-
-
-}
